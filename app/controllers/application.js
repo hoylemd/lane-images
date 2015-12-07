8f63faff8723622b54caa920c7dc3e16f53ab50a
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-07 14:49:40 -0500

    add action classes

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,17 +3,17 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   image_source: 'http://i.imgur.com/LmfujBz.png',
   available_actions: [
-    {name:'Rotate Right', class_to_apply:''},
-    {name:'Rotate Left', class_to_apply:''},
-    {name:'Rotate 180', class_to_apply:''},
-    {name:'Translate Left', class_to_apply:''},
-    {name:'Translate Right', class_to_apply:''},
-    {name:'Translate Up', class_to_apply:''},
-    {name:'Translate Down', class_to_apply:''},
-    {name:'Scale to Half', class_to_apply:''},
-    {name:'Scale to Double', class_to_apply:''},
-    {name:'50% Opacity', class_to_apply:''}
-  ],
+    {name:'Rotate Right', class_to_apply:'rotate_right'},
+    {name:'Rotate Left', class_to_apply:'rotate_left'},
+    {name:'Rotate 180', class_to_apply:'rotate_180'},
+    {name:'Translate Left', class_to_apply:'translate_left'},
+    {name:'Translate Right', class_to_apply:'translate_right'},
+    {name:'Translate Up', class_to_apply:'translate_up'},
+    {name:'Translate Down', class_to_apply:'translate_down'},
+    {name:'Scale to Half', class_to_apply:'scale_half'},
+    {name:'Scale to Double', class_to_apply:'scale_double'},
+    {name:'Half Opacity', class_to_apply:'half_opacity'}
+  ]
   applied_actions: [
   ]
 });
