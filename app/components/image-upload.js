08a1e49d661428060258c794975c19eca778618c
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-16 13:34:48 -0500

    Change some class names and default parameters
    I add a descriptive default for the component action, so I don;t need to
    specify it anymore.  I also made the idefault upload input class more
    explicit, to make collisions more unlikely, and also made it
    configurable in case it ever needs to be something different.

--- app/components/image-upload.js
+++ app/components/image-upload.js
@@ -3,8 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   file_input: null,
 
+  // default action name
+  action: "upload_image",
+
+  // default input class name
+  input_class: 'js-image-upload-input',
+
+  file_input: function() {
+    return Ember.$('.' + this.get('input_class'))[0];
+  }.property('input_class'),
+
   setup: function() {
-    var input = Ember.$('.js-image-upload')[0],
+    var input = this.get('file_input'),
       handler = this.get('file_input_change_handler');
     input.addEventListener("change", handler);
     this.set('file_input', input);
